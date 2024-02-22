e4580bed67622e0b1ee704c74654f9ce1f211a23
saurabhsadhu30 <99708532+saurabhsadhu30@users.noreply.github.com>
2024-02-22 12:15:12 +0530

    Update index.js

--- client/src/requests/index.js
+++ client/src/requests/index.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export const server = location.href.includes("localhost")
   ? "http://127.0.0.1:8080"
-  : "https://vercel.com/saurabhsadhu2000-gmailcom/final-test-quiz";
+  : "https://quizzie-api-cozd.onrender.com";
 
 export function config() {
   const token = Cookies.get("token");
