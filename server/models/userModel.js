42289b528d8b1e8b4160bbbd5d23b3b539c940d5
saurabhsadhu30 <99708532+saurabhsadhu30@users.noreply.github.com>
2024-02-22 23:54:35 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      minLength: [3, "Password must be at least 6 characters long"],
+      minLength: [3, "Password must be at least 3 characters long"],
       required: true,
       select: false,
     },
